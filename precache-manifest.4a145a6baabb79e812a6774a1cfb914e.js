a9143932286e00fc209892206242746776370f67
robe007 <rcarlos007@gmail.com>
2020-01-08 15:46:47 -0600

    Updates

---
+++ precache-manifest.4a145a6baabb79e812a6774a1cfb914e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e18f8e70d4b91cb50b07d91b60bc19",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06c4cb6fbcf80a18aa94",
+    "url": "/monsters-rolodex/static/css/main.b280d5ad.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06c4cb6fbcf80a18aa94",
+    "url": "/monsters-rolodex/static/js/main.976cac63.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
